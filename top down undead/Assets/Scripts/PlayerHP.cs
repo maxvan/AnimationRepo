bf4ce6cbe39267194da99408f5e835e9f2cedecc
Max Van Den Dikkenberg <21.Max.Van@ksd.org>
2019-10-17 10:29:03 -0700

    Animation

---
+++ top down undead/Assets/Scripts/PlayerHP.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class PlayerHP : MonoBehaviour
+{
+    public int health = 3;
+    public Text healthText;
+    public Slider HealthBar;
+    public int lives;
+    private void Start()
+    {
+        healthText.text = "HP: " + health;
+        HealthBar.maxValue = health;
+        HealthBar.value = health;
+        lives = PlayerPrefs.GetInt("lives");
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            health = health - 1;
+            healthText.text = "HP: " + health;
+            HealthBar.value = health;
+            if (health < 1)
+            {
+                if (lives > 0)
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                    PlayerPrefs.SetInt("lives", lives - 1);
+                }
+                else
+                {
+                    SceneManager.LoadScene("gameover");
+                }
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "EnemyBullet")
+        {
+            health = health - 1;
+            healthText.text = "HP: " + health;
+            HealthBar.value = health;
+            if (health < 1)
+            {
+                if (lives > 0)
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                    PlayerPrefs.SetInt("lives", lives - 1);
+                }
+                else
+                {
+                    SceneManager.LoadScene("gameover");
+                }
+            }
+
+        }
+    }
+}
